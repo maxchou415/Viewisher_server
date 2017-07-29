23756e4f49ecd704791d48b4118409a6ac33dd84
maxyihsunchou <yihsunmaxchou@icloud.com>
2017-07-30 04:51:36 +0800

    api

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,8 +2,4 @@ const router = require('koa-router')()
 
 router.prefix('/dashboard')
 
-router.get('/', async (ctx, next) => {
-  ctx.body = 'this is a users response!'
-})
-
 module.exports = router
